73d21d13b6e778040a2ec68d443ddd1b1d346747
Muhammad Asif <bs170200665@vu.edu.pk>
2022-03-02 19:17:25 +0500

    finala

---
+++ src/pages/About.jsx
@@ -0,0 +1,42 @@
+/** @format */
+
+import React from "react";
+import Layout from "../components/Globle/Layout";
+
+const About = () => {
+	return (
+		<Layout>
+			<section className="content-info py-5" id="about">
+				<div className="container py-md-5">
+					<h3 className="heading text-center mb-3 mb-sm-5">About us</h3>
+
+					<div className="info-w3pvt-mid text-center px-lg-5">
+						<div className="title-desc text-center px-lg-5">
+							<img
+								src="assets/images/about1.png"
+								alt="news image"
+								className="img-fluid"
+							/>
+							<p className="px-lg-5">
+								Praesent ullamcorper dui turpis.At vero eos et accusam et justo
+								duo dolores et ea rebum.Integer sit amet mattis quam, sit amet
+								ultricies velit. Praesent ullamcorper dui turpis. Praesent
+								ullamcorper dui turpis.At vero eos et accusam et justo duo
+								dolores et ea rebum.Integer sit amet mattis quam, sit amet
+								ultricies velit. Praesent ullamcorper dui turpis.
+							</p>
+							{/* <a
+							className="btn mt-lg-4 mt-3 read scroll"
+							href="#services"
+							role="button">
+							Learn More
+						</a> */}
+						</div>
+					</div>
+				</div>
+			</section>
+		</Layout>
+	);
+};
+
+export default About;
