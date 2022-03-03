09177c6edb43162e05ef8eb36de9f70c9cae7ba3
Muhammad Asif <bs170200665@vu.edu.pk>
2022-03-03 19:29:10 +0500

    final update

--- src/Config.jsx
+++ src/Config.jsx
@@ -81,115 +81,31 @@ const Config = () => {
 	// console.log(getDate);
 	return (
 		<>
-		<Weather_info 
-		title
-		
-		
-		
-		
-		
-		
-		>
-
-
+			<Weather_info
+				title="Find current weather condition"
+				city={city.name}
+				country={city == "" ? "PK" : city.sys.country}
+				date={getDate}
+				time={getTime}
+				icon={
+					city == ""
+						? { Icon }
+						: `http://openweathermap.org/img/w/${city.weather[0].icon}.png`
+				}
+				weather={city == "" ? getval : city.weather[0].main}
+				temp={city == "" ? getval : city.main.temp}
+				pressure={city == "" ? getval : city.main.pressure}
+				temp_min={city == "" ? getval : city.main.temp_min}
+				humidity={city == "" ? getval : city.main.humidity}
+				feels_like={city == "" ? getval : city.main.feels_like}
+				Wind={city == "" ? getval : city.wind.speed}
+				degree={city == "" ? getval : city.wind.deg}>
+				<input type="text" placeholder="Search" onChange={searchValue} />
+				<button type="submit" className="search_btn" onClick={onSearch}>
+					Search Weather
+				</button>
 			</Weather_info>
-			<div>
-			<div className="name container">
-				<div className="row">
-					<div className="col-12">
-						<h1 className="pb-2 text-capitalize">
-							Find current weather condition
-						</h1>
-						<div className="search">
-							<input type="text" placeholder="Search" onChange={searchValue} />
-							<button type="submit" className="search_btn" onClick={onSearch}>
-								Search Weather
-							</button>
-							{/* {city} */}
-							{/* {getDate} */}
-						</div>
-						{/* {error} */}
-						<div className="  p-5">
-							<div className="col-12 weather-info pr-5 col-sm-12 col-md-12 text-center">
-								<h2>
-									{apierror}
-									{city.name},
-									<span> {city == "" ? "PK" : city.sys.country}</span>
-								</h2>
-								<div className="date">
-									<h5>{getDate}</h5>
-									<h5>{getTime}</h5>
-								</div>
-								<div className=" ">
-									<i className="fa fa-solid fa-sun"></i>
-									<div className="centig">
-										<img
-											src={
-												city == ""
-													? { Icon }
-													: `http://openweathermap.org/img/w/${city.weather[0].icon}.png`
-											}
-											alt=""
-										/>
-										{/* <img src={icon} alt="" /> */}
-										<h2>
-											{city == "" ? getval : city.main.temp}
-											<span> °F</span>{" "}
-										</h2>
-
-										<p>{city == "" ? getval : city.weather[0].main}</p>
-									</div>
-								</div>
-							</div>
-							<div className="col-12 col-sm-12 weather-info col-md-12 border-right-0">
-								<div className=" ">
-									<div className="detail p-3">
-										<Temp
-											value={city == "" ? getval : city.main.pressure}
-											station="pressure"
-										/>
-										<Temp
-											value={city == "" ? getval : city.main.humidity}
-											station="humidity"
-										/>
-										<Temp
-											value={city == "" ? getval : city.main.temp_min}
-											station="temp_min"
-										/>
-
-										<Temp
-											value={city == "" ? getval : city.main.feels_like}
-											station="feels_like"
-										/>
-
-										<Temp
-											value={city == "" ? getval : city.wind.speed}
-											station="Wind speed"
-										/>
-										<Temp
-											value={city == "" ? getval : city.wind.deg}
-											station="Wind degree"
-										/>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<i className="fa fa-solid fa-cloud" aria-hidden="true"></i>
-
-						{/* <p>{error}</p> */}
-					</div>
-				</div>
-
-				{/* <p>{date}</p> */}
-				{/* <p className="c-name">
-					{city == "" ? getval : city.name},
-					<span> {city == "" ? "PK" : city.sys.country}</span>
-				</p> */}
-			</div>
-		</div>
 		</>
-		
 	);
 };
 
